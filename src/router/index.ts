ca6eab3a97f03d5ac2c8901753e0ea5fd87ed87d
rkroom <53672435+rkroom@users.noreply.github.com>
2021-08-14 21:33:06 +0800

    Update index.ts
    change route mode to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
